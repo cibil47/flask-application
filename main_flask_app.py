48ffb7fd3d75412ae11651bc63979774dfcc9787
cibil47 <34987282+cibil47@users.noreply.github.com>
2019-04-04 19:56:10 +0530

    Add files via upload
    Adding initial files

---
+++ main_flask_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template,url_for
+
+app = Flask(__name__)
+
+posts = [
+        {"author": "Jason",
+         "title": "Artificial Intelligence",
+         "posted_date":"Today",
+         "content":"Something about artificial inelligence"
+         },
+        {"author": "Brody",
+         "title": "Machine learning",
+         "posted_date":"Yesterday",
+         "content":"Machine learning is a sub field of artificial intelligence"
+         },
+        {"author": "Blog Bot",
+         "title": "Deep learning",
+         "posted_date":"Day before yesterday",
+         "content":"Deep learning is a sub field of machine learning. We can do some cool stuff with deep learning"
+         },
+        ]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html",  posts = posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html",title = "About")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
