68266e02599bed6ab31f8072a188cf441d0fbf77
cibil47 <cibil51@gmail.com>
2019-05-06 15:46:27 +0530

    added db and login,logout routes

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
+from application.models import User
 
 class RegistrationForm(FlaskForm):
 
@@ -10,6 +10,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(),EqualTo("password")])
     submit = SubmitField("Register")
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username is already taken.Plaese choose another one")
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is already taken.Plaese choose another one")
 
 class LoginForm(FlaskForm):
 
